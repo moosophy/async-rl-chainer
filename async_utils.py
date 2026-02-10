a6d7c0a23116cc0ab0a6c816189ead8893fffb3e
moosophy <yekaterinayak@gmail.com>
2026-02-10 14:03:28 +0100

    optimizer brute fix

--- async_utils.py
+++ async_utils.py
@@ -22,22 +22,24 @@ def set_shared_params(a, b):
                 shared_param, dtype=param.data.dtype).reshape(param.data.shape)
 
 
-def set_shared_states(a, b):
-    assert isinstance(a, chainer.Optimizer)
-    assert hasattr(a, 'target'), 'Optimizer.setup must be called first'
-    for state_name, shared_state in b.items():
-        for param_name, param in shared_state.items():
-            old_param = a._states[state_name][param_name]
-            a._states[state_name][param_name] = np.frombuffer(
-                param,
-                dtype=old_param.dtype).reshape(old_param.shape)
+def set_shared_states(optimizer, shared_arrays):
+    import numpy as np
+
+    for idx, shared_state in shared_arrays.items():
+        for key, raw_array in shared_state.items():
+            old_param = optimizer._states[idx][key]
+            optimizer._states[idx][key] = np.frombuffer(
+                raw_array,
+                dtype=old_param.dtype
+            ).reshape(old_param.shape)
+
 
 
 def extract_params_as_shared_arrays(link):
     assert isinstance(link, chainer.Link)
     shared_arrays = {}
     for param_name, param in link.namedparams():
-        shared_arrays[param_name] = mp.RawArray('f', param.data.ravel())
+        shared_arrays[id(param)] = mp.RawArray('f', param.data.ravel())
     return shared_arrays
 
 
@@ -56,23 +58,25 @@ def share_states_as_shared_arrays(link):
 #FUNCTION BELOW IS UPDATED FOR CHAINER 7.8.1
 def extract_states_as_shared_arrays(optimizer):
     import multiprocessing as mp
-    import chainer
 
-    assert isinstance(optimizer, chainer.Optimizer)
-    assert hasattr(optimizer, 'target'), 'Optimizer.setup must be called first'
-
-    model = optimizer.target  # this is the link/model
+    model = optimizer.target
     shared_arrays = {}
 
     for idx, param in enumerate(model.params()):
         state = param.update_rule.state
         if state is None:
-            continue  # skip parameters without state
+            continue
+
         shared_arrays[idx] = {}
+
         for key, value in state.items():
-            shared_arrays[idx][key] = mp.RawArray('f', value.ravel())
+            shared_arrays[idx][key] = mp.RawArray(
+                'f',
+                value.ravel()
+            )
 
     return shared_arrays
+
 
 
 def run_async(n_process, run_func):
--- rmsprop_async.py
+++ rmsprop_async.py
@@ -1,38 +1,51 @@
-import numpy
-
+import numpy as np
 from chainer import cuda
 from chainer import optimizer
 
-
 class RMSpropAsync(optimizer.GradientMethod):
+    """RMSprop for asynchronous A3C with shared-memory support."""
 
-    """RMSprop for asynchronous methods.
-    
-    The only difference from chainer.optimizers.RMSprop in that the epsilon is
-    outside the square root."""
-
-    def __init__(self, lr=0.01, alpha=0.99, eps=1e-8):
+    def __init__(self, lr=7e-4, alpha=0.99, eps=1e-8):
+        super().__init__()
         self.lr = lr
         self.alpha = alpha
         self.eps = eps
+        self._states = {}  # <- needed by async code
+        
 
-    def init_state(self, param, state):
-        xp = cuda.get_array_module(param.data)
-        state['ms'] = xp.zeros_like(param.data)
+    def setup(self, link):
+        super().setup(link)
+        for idx, param in enumerate(link.params()):
+            param.update_rule = self.create_update_rule()
+            param.update_rule.init_state(param)
+            self._states[idx] = param.update_rule.state
+        return self
 
-    def update_one_cpu(self, param, state):
-        ms = state['ms']
-        grad = param.grad
+    def create_update_rule(self):
+        class Rule:
+            def __init__(self, lr, alpha, eps):
+                self.lr = lr
+                self.alpha = alpha
+                self.eps = eps
+                self.state = None
 
-        ms *= self.alpha
-        ms += (1 - self.alpha) * grad * grad
-        param.data -= self.lr * grad / numpy.sqrt(ms + self.eps)
+            def init_state(self, param):
+                xp = cuda.get_array_module(param.data)
+                self.state = {'ms': xp.zeros_like(param.data)}
 
-    def update_one_gpu(self, param, state):
-        cuda.elementwise(
-            'T grad, T lr, T alpha, T eps',
-            'T param, T ms',
-            '''ms = alpha * ms + (1 - alpha) * grad * grad;
-               param -= lr * grad / sqrt(ms + eps);''',
-            'rmsprop')(param.grad, self.lr, self.alpha, self.eps,
-                       param.data, state['ms'])
+            def update_one_cpu(self, param):
+                ms = self.state['ms']
+                grad = param.grad
+                ms *= self.alpha
+                ms += (1 - self.alpha) * grad * grad
+                param.data -= self.lr * grad / np.sqrt(ms + self.eps)
+
+            def update_one_gpu(self, param):
+                ms = self.state['ms']
+                grad = param.grad
+                xp = cuda.get_array_module(param.data)
+                ms *= self.alpha
+                ms += (1 - self.alpha) * grad * grad
+                param.data -= self.lr * grad / xp.sqrt(ms + self.eps)
+
+        return Rule(self.lr, self.alpha, self.eps)
